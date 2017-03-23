0d84fd8b9b7cbe8fc347aa5c2cc0594df59a31d5
azakbari <azakbari@scs.ryerson.ca>
2017-03-23 17:42:21 -0400

    added

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,6 @@ var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -26,6 +25,41 @@ router.get('/get-data', function(req, res, next) {
     });
   });
 });
+
+
+router.get('/get-ingredients', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('ingredients').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+
+router.post('/something', function(req, res, next) {
+
+  var something = req.body.something;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('ingredients').insertOne(something, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+
 
 router.post('/insert', function(req, res, next) {
   var item = {
