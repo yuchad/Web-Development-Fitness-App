caa425421a6d955ae349d3f3e35b61a98aabf856
Mahmudulla Fahim <mmfahim@ryerson.ca>
2017-03-30 17:22:11 -0400

    yall got blessed

--- apiCallWorking/app/public/js/test.js
+++ apiCallWorking/app/public/js/test.js
@@ -1,52 +1,79 @@
-
 function doIt(ingredients) {
- var output = $.ajax({
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=' + ingredients + '&limitLicense=false&number=5&ranking=1', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) {
-    	//
-        //Change data.source to data.something , where something is whichever part of the object you want returned.
-        //To see the whole object you can output it to your browser console using:
-        //console.log(data);
+    var recipeID, recipeTitle;
+    var output = $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=' + ingredients + '&limitLicense=false&number=10&ranking=1', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            for(var i = 0; i < data.length; i++){
+                recipeID = data[i].id;
+                var recipe = $.ajax({
+                    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/'+ recipeID +'/information?includeNutrition=true', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+                    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+                    data: {}, // Additional parameters here
+                    dataType: 'json',
+                    success: function(data) {
+                        //console.log(data);
+                        var recipeDIV = document.createElement("div");
 
-       	document.getElementById("output").innerHTML = data[0].title;
-       	document.getElementById("outputID").innerHTML = data[0].id;
-       	console.log(data);
+                        var name = document.createElement("h4");
+                        var node = document.createTextNode(data.title);
+                        name.appendChild(node);
+                        name.setAttribute('id','recipeName');
+                        recipeDIV.appendChild(name);
+
+                        var calories = document.createElement("p");
+                        var node = document.createTextNode("Calories: " + data.nutrition.nutrients[0].amount);
+                        calories.appendChild(node);
+                        calories.setAttribute('id','calories');
+                        recipeDIV.appendChild(calories);
+
+                        var recipeImage = document.createElement("img");
+                        recipeImage.setAttribute('src',data.image);
+                        recipeImage.setAttribute('id','image');
+                        recipeDIV.appendChild(recipeImage);
+
+                        var recipeLink = document.createElement("a");
+                        recipeLink.setAttribute('href',data.sourceUrl);
+                        recipeLink.setAttribute('id','link');
+                        recipeLink.innerHTML = "View Recipe";
+                        recipeDIV.appendChild(recipeLink);
+
+                        var fav = document.createElement("BUTTON");
+                        fav.setAttribute('id','favorite');
+                        fav.setAttribute('onclick','addFav();');
+                        fav.onclick = function(){addFav(data.id);};
+                        fav.innerHTML = "Add to Favorite";
+                        recipeDIV.appendChild(fav);
+                        recipeDIV.setAttribute('id','recipeInfo');
+
+                        var br = document.createElement("BR");
+                        recipeDIV.appendChild(br);
+                        var br = document.createElement("BR");
+                        recipeDIV.appendChild(br);
+                        var br = document.createElement("BR");
+                        recipeDIV.appendChild(br);
+
+                        var element = document.getElementById("recipes");
+                        element.appendChild(recipeDIV);
+                    },
+                    error: function(err) { alert(err); },
+                    beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-Mashape-Authorization", "Iq8WHFs6VomshBya8vdSeZU7kL7mp1KlOLFjsnuKWX7TjWyclC"); // Enter here your Mashape key
+                    }
+                });
+            }
         },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "Iq8WHFs6VomshBya8vdSeZU7kL7mp1KlOLFjsnuKWX7TjWyclC"); // Enter here your Mashape key
-    }
-});
-
- var recipe = $.ajax({
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/420337/information?includeNutrition=false', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) {
-    	//
-        //Change data.source to data.something , where something is whichever part of the object you want returned.
-        //To see the whole object you can output it to your browser console using:
-        //console.log(data);
-
-        document.getElementById("recipe").innerHTML = data.readyInMinutes;
-       	console.log(data);
-        },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "Iq8WHFs6VomshBya8vdSeZU7kL7mp1KlOLFjsnuKWX7TjWyclC"); // Enter here your Mashape key
-    }
-});
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", "Iq8WHFs6VomshBya8vdSeZU7kL7mp1KlOLFjsnuKWX7TjWyclC"); // Enter here your Mashape key
+        }
+    });
 }
 
-function getIng(){
- var ingredient = document.getElementById("ing").value;
-
- var ingredients = [];
- ingredients.push(ingredient);
-
- console.log(ingredients);
- }
+var favList = [];
+function addFav(id){
+    favList.push(id);
+    console.log(favList);
+}
